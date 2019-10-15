b17327745804f0ee1e293b32c28534f981cd8278
MK <kaliszmat1@gmail.com>
2019-10-15 17:51:15 +0200

    easy hamburger menu effect lesson

--- part64/main.js
+++ part64/main.js
@@ -1,5 +1,11 @@
 const burger = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.fa-bars');
+const burgerX = document.querySelector('.fa-times');
+const aside = document.querySelector('aside');
+
 
 burger.addEventListener('click', ()=>{
-    burger.classList.toggle('active');
+    burgerMenu.classList.toggle('active');
+    burgerX.classList.toggle('active');
+    aside.classList.toggle('asideMenu');
 })
