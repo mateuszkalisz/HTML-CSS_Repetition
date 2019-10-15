b7e40933ea8d1ba170c0d9b900584b02ef63c3b3
MK <kaliszmat1@gmail.com>
2019-10-15 21:34:38 +0200

    code improvement by JS

--- part64/main.js
+++ part64/main.js
@@ -1,13 +1,24 @@
 const burger = document.querySelector('.burger');
-const burgerMenu = document.querySelector('.fa-bars');
-const burgerX = document.querySelector('.fa-times');
-const aside = document.querySelector('aside');
-const wrapper = document.querySelector('.wrapper');
+// const burgerMenu = document.querySelector('.fa-bars');
+// const burgerX = document.querySelector('.fa-times');
+// const aside = document.querySelector('aside');
+// const wrapper = document.querySelector('.wrapper');
 
 
-burger.addEventListener('click', ()=>{
-    burgerMenu.classList.toggle('active');
-    burgerX.classList.toggle('active');
-    aside.classList.toggle('active');
-    wrapper.classList.toggle('active');
-})
+// burger.addEventListener('click', ()=>{
+//     burgerMenu.classList.toggle('active');
+//     burgerX.classList.toggle('active');
+//     aside.classList.toggle('active');
+//     wrapper.classList.toggle('active');
+// })
+
+const current = document.querySelectorAll('.current');
+
+const currentArray = [...current];
+
+burger.addEventListener('click', function(){
+    currentArray.forEach(item =>{
+        item.classList.toggle('active');
+    })
+});
+
