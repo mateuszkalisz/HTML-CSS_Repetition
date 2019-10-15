8df66e3a58a2cbe5d140c6a3cc7bd5638d814d29
MK <kaliszmat1@gmail.com>
2019-10-15 21:29:02 +0200

    adjust content to hamburger menu by JS

--- part64/main.js
+++ part64/main.js
@@ -2,10 +2,12 @@ const burger = document.querySelector('.burger');
 const burgerMenu = document.querySelector('.fa-bars');
 const burgerX = document.querySelector('.fa-times');
 const aside = document.querySelector('aside');
+const wrapper = document.querySelector('.wrapper');
 
 
 burger.addEventListener('click', ()=>{
     burgerMenu.classList.toggle('active');
     burgerX.classList.toggle('active');
-    aside.classList.toggle('asideMenu');
+    aside.classList.toggle('active');
+    wrapper.classList.toggle('active');
 })
